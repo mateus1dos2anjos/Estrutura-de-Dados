2163f2c03c6a34c11c712e290e34608a01a680d6
mateus1dos2anjos <mateus.16745@gmail.com>
2022-10-27 17:07:17 -0300

    Trabalhos

---
+++ Trabalho_8_Arvore_Binaria_Busca/main.cpp
@@ -0,0 +1,4 @@
+/*
+Implemente um TAD para representar uma Árvore Binária de Busca. Deve-se tentar evitar
+a formação de árvores degeneradas. Esse TAD deverá conter funções para:
+*/
---
+++ Trabalho_9_Arvores_AVL/main.cpp
@@ -0,0 +1,4 @@
+/*
+Implemente um TAD para representar uma Árvore AVL. Deve-se tentar evitar a formação
+de árvores degeneradas. Esse TAD deverá conter funções para:
+*/
